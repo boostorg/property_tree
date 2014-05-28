1c6747a83f037d5ae4936fafaafbcd181ecbcd80
CornedBee <sebastian.redl@getdesigned.at>
2014-05-28 11:35:04 +0200

    Clone v1 as v2.

---
+++ include/boost/property_tree/v2/detail/info_parser_utils.hpp
@@ -0,0 +1,33 @@
+// ----------------------------------------------------------------------------
+// Copyright (C) 2002-2006 Marcin Kalicinski
+//
+// Distributed under the Boost Software License, Version 1.0. 
+// (See accompanying file LICENSE_1_0.txt or copy at 
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+// For more information, see www.boost.org
+// ----------------------------------------------------------------------------
+#ifndef BOOST_PROPERTY_TREE_V2_DETAIL_INFO_PARSER_CHCONV_HPP_INCLUDED
+#define BOOST_PROPERTY_TREE_V2_DETAIL_INFO_PARSER_CHCONV_HPP_INCLUDED
+
+#include <boost/property_tree/version.hpp>
+#include <string>
+
+BOOST_PROPERTY_TREE_OPENNS(2) namespace info_parser
+{
+
+    template<class ChDest, class ChSrc>
+    std::basic_string<ChDest> convert_chtype(const ChSrc *text)
+    {
+        std::basic_string<ChDest> result;
+        while (*text)
+        {
+            result += ChDest(*text);
+            ++text;
+        }
+        return result;
+    }
+
+} BOOST_PROPERTY_TREE_CLOSENS()
+
+#endif
---
+++ include/boost/property_tree/v2/detail/xml_parser_flags.hpp
@@ -0,0 +1,32 @@
+// ----------------------------------------------------------------------------
+// Copyright (C) 2002-2006 Marcin Kalicinski
+//
+// Distributed under the Boost Software License, Version 1.0. 
+// (See accompanying file LICENSE_1_0.txt or copy at 
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+// For more information, see www.boost.org
+// ----------------------------------------------------------------------------
+#ifndef BOOST_PROPERTY_TREE_V2_DETAIL_XML_PARSER_FLAGS_HPP_INCLUDED
+#define BOOST_PROPERTY_TREE_V2_DETAIL_XML_PARSER_FLAGS_HPP_INCLUDED
+
+#include <boost/property_tree/version.hpp>
+
+BOOST_PROPERTY_TREE_OPENNS(2) namespace xml_parser {
+
+    /// Text elements should be put in separate keys,
+    /// not concatenated in parent data.
+    static const int no_concat_text  = 0x1;
+    /// Comments should be omitted.
+    static const int no_comments     = 0x2;
+    /// Whitespace should be collapsed and trimmed.
+    static const int trim_whitespace = 0x4;
+
+    inline bool validate_flags(int flags)
+    {
+        return (flags & ~(no_concat_text | no_comments | trim_whitespace)) == 0;
+    }
+
+} BOOST_PROPERTY_TREE_CLOSENS()
+
+#endif
