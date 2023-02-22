17e4c9fefd1b0b36141ad1acc6dc70dd251b959a
Ed Catmur <edward.catmur@mavensecurities.com>
2023-02-22 19:40:34 +0000

    Use BOOST_DEFAULTED_FUNCTION on empty destructors
    The compiler-generated copy constructor and copy assignment operator are deprecated since C++11 on classes with user-declared destructors.
    
    This change allows clean compilation with the -Wdeprecated-copy-dtor/-Wdeprecated-copy-with-user-provided-dtor flag.

--- include/boost/property_tree/detail/file_parser_error.hpp
+++ include/boost/property_tree/detail/file_parser_error.hpp
@@ -34,11 +34,11 @@ namespace boost { namespace property_tree
         {
         }
 
-        ~file_parser_error() throw() BOOST_OVERRIDE
+        BOOST_DEFAULTED_FUNCTION(~file_parser_error() throw() BOOST_OVERRIDE,
             // gcc 3.4.2 complains about lack of throw specifier on compiler
             // generated dtor
         {
-        }
+        })
 
         ///////////////////////////////////////////////////////////////////////
         // Data access
